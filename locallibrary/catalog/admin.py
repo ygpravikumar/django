c0bb58baac9dafcc4469adc8f50c41326b589555
Ravi Kumar G <ravikumarygp.mahesh@gmail.com>
2017-06-05 01:26:14 +0530

    Skeleton locallibrary project with catalog application

---
+++ locallibrary/catalog/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+admin.site.register(Book, BookAdmin)
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Genre, GenreAdmin)
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+admin.site.register(Language)
---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,12 @@
+"""
+Define the url mapping for catalog app
+
+"""
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+
+]
